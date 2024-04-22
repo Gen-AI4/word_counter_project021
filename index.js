7236a00204abeaab18fe0bf11cf5734be364b2dd
Ahmed Ali <160950887+Gen-AI4@users.noreply.github.com>
2024-04-22 16:38:43 +0500

    This is a word counter project.

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const sentence = await inquirer.prompt([
+    {
+        name: "message",
+        type: "input",
+        message: "Enter your sentence for count words!",
+    },
+]);
+const message = sentence.message.trim().split(" ");
+console.log(message);
+console.log(`Your sentence word count is ${message.length}`);
